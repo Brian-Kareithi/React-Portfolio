50272a1f2a5c7bb5d18c1cab24b8d495927992a0
Brian Kareithi <kareithibrian1@gmail.com>
2026-01-09 23:25:22 +0300

    Adding layout client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,20 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import DynamicBackground from "./components/DynamicBackground";
-
-import Navbar from "./components/NavBar";
-import ScrollBar from "./components/ScrollBar";
-import LoadingScreen from "./components/LoadingScreen";
-
 import "./globals.css";
+import LayoutClient from "./LayoutClient";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [isLoading, setIsLoading] = useState(true);
+export const metadata = {
+  title: "Brian Kareithi",
+  description: "Brian Kareithi - Fullstack Dev & Cybersecurity Enthusiast",
+};
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head>
-        <title>Brian Kareithi</title>
-        <meta name="description" content="Brian Kareithi - Fullstack Dev & Cybersecurity Enthusiast" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>BK</text></svg>" />
-      </head>
       <body className="relative text-white overflow-x-hidden bg-gradient-to-b from-black to-blue-900">
-        <LoadingScreen isLoading={isLoading} />
-        
-        <div className={`transition-opacity duration-600 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
-          <DynamicBackground />
-          <Navbar />
-          <ScrollBar />
-        </div>
-
-        <main className={`relative z-10 transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
-          {children}
-        </main>
+        <LayoutClient>{children}</LayoutClient>
       </body>
     </html>
   );
