3d7981fc757dada00786c941333a58bb38c49f39
Brian-Kareithi <kareithibrian1@gmail.com>
2026-01-09 23:39:15 +0300

    Bringing Back the loading screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import DynamicBackground from "./components/DynamicBackground";
 
 import Navbar from "./components/NavBar";
 import ScrollBar from "./components/ScrollBar";
+import LoadingScreen from "./components/LoadingScreen";
 
 import "./globals.css";
 
@@ -27,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>BK</text></svg>" />
       </head>
       <body className="relative text-white overflow-x-hidden bg-gradient-to-b from-black to-blue-900">
-
+        <LoadingScreen isLoading={isLoading} />
         
         <div className={`transition-opacity duration-600 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
           <DynamicBackground />
